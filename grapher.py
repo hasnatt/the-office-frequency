65a9b8a08a7fe4cb52dcd1bfcc982e247ad29f74
Hasnat Abdul <hasnatabdul5@gmail.com>
2021-07-17 03:18:07 +0100

    Add seaborn plotting method

--- grapher.py
+++ grapher.py
@@ -1,7 +1,11 @@
 import json
 from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+
 
 # TODO: Implement a class and start using static and class methods
+
 
 with open('full_frequency.json') as json_file:
     data = json.load(json_file)
@@ -17,17 +21,41 @@ for episode in data:
 
         print(f"{episode['s']}x{episode['e']}: {episode['frequencies']['Michael']}")
         words_list.append(int(episode['frequencies']['Michael']))
-        episodes_list.append(f"{episode['s']}x{episode['e']}")
+        episodes_list.append(f"{episode['s']}{episode['e']}")
     
     else:
         print(f"{episode['s']}x{episode['e']}: 0")    
         words_list.append(0)
         episodes_list.append(f"{episode['s']}x{episode['e']}")
-    
+
+
+
+data = {'ep': episodes_list, 'words': words_list}
+#convert dictionary to a dataframe
+df = pd.DataFrame(data)
+#Print out all rows        
+print(df)    
 
 plt.style.use('fivethirtyeight')
-plt.figure(figsize=(18, 3))
-plt.grid(b=None)
-plt.plot(counter_list, words_list)
+# plt.figure(figsize=(36, 6))
+# # plt.grid(b=None)
+# # plt.axis(False)
+# plt.plot(counter_list, words_list, linewidth=12)
+# plt.savefig("test.png", dpi = 400, transparent=True)
 
+
+
+# fig = plt.figure()
+# ax = fig.add_axes([0,0,1,1])
+# ax.bar(episodes_list,words_list)
+# plt.show()
+
+
+
+plt.figure(figsize=(36, 6))
+plt.fill_between(df.ep, df.words, color='tab:brown')
+
+plt.axis(False)
+sns.lineplot(x='ep', y='words', data=df, sort=False, linewidth=4)
 plt.show()
+
