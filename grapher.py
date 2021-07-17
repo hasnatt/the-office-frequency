2d61a268c9174c18f48e64d2e5ef296ff28a8e93
Hasnat Abdul <hasnatabdul5@gmail.com>
2021-07-17 02:23:08 +0100

    add simple temp plotting of words

--- grapher.py
+++ grapher.py
@@ -1,10 +1,31 @@
 import json
+from matplotlib import pyplot as plt
 
 with open('full_frequency.json') as json_file:
     data = json.load(json_file)
 
+words_list = list()
+episodes_list = list()
+counter = 0 
+counter_list=list()
 for episode in data:
+    counter_list.append(counter)
+    counter +=1    
     if "Michael" in episode['frequencies'].keys():
+
         print(f"{episode['s']}x{episode['e']}: {episode['frequencies']['Michael']}")
+        words_list.append(int(episode['frequencies']['Michael']))
+        episodes_list.append(f"{episode['s']}x{episode['e']}")
+    
     else:
         print(f"{episode['s']}x{episode['e']}: 0")    
+        words_list.append(0)
+        episodes_list.append(f"{episode['s']}x{episode['e']}")
+    
+
+plt.style.use('fivethirtyeight')
+plt.figure(figsize=(18, 3))
+plt.grid(b=None)
+plt.plot(counter_list, words_list)
+
+plt.show()
