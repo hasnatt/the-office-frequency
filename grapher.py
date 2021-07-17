2f18ac7e452b22e8ce044d171589c909d7d62b13
Hasnat Abdul <hasnatabdul5@gmail.com>
2021-07-17 01:32:04 +0100

    plan plotting of data

--- grapher.py
+++ grapher.py
@@ -1,1 +1,10 @@
-import os
+import json
+
+with open('full_frequency.json') as json_file:
+    data = json.load(json_file)
+
+for episode in data:
+    if "Michael" in episode['frequencies'].keys():
+        print(f"{episode['s']}x{episode['e']}: {episode['frequencies']['Michael']}")
+    else:
+        print(f"{episode['s']}x{episode['e']}: 0")    
