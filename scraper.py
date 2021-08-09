c1b7b8f8a4fd64a3089d1215ee51c7dade5f23a7
Hasnat Abdul <hasnatabdul5@gmail.com>
2021-08-09 23:19:30 +0100

    Pylinting scraper

--- scraper.py
+++ scraper.py
@@ -1,35 +1,43 @@
+"""Scraper module"""
 from bs4 import BeautifulSoup
-import requests, re, string, json, time
+import requests
+import re
+import json
+import time
 from collections import Counter
 
 class Scraper:
+    """Scraping the office transcript class """
     def __init__(self, url):
         self.url = url
 
 
     def get_html(self):
+        """Return the HTML contents of the webpage with lxml parser"""
         source = requests.get(self.url).text
         soup = BeautifulSoup(source, 'lxml')
-        return(soup)
+        return soup
 
 
     def is_deleted_scenes(self):
+        """Check if transcript page is deleted scenes episode, return bool"""
         soup = self.get_html()
         title = soup.title.text
         substring = 'Deleted Scenes'
         if substring in title:
-            return(True)
-        else:
-            return(False)
+            return True
+        return False
 
 
     def get_title(self):
+        """Return episode title in the title tag"""
         soup = self.get_html()
         title = soup.title.text.split(' - ')
-        return(title[1].strip()) 
+        return title[1].strip()
 
 
     def get_season_and_episode(self):
+        """Return the season and episode defined in the webpage"""
         soup = self.get_html()
         title = soup.title.text.split('-')
         season_title = title[0].split('x')
@@ -40,12 +48,13 @@ class Scraper:
 
 
     def get_transcript(self):
+        """Gather the word count for each character in the given episode"""
         soup = self.get_html()
         script = soup.find('div', class_='postbody')
         script_list = []
         for line in script.select('p'):
             line = line.text
-            line = re.sub("[\(\[].*?[\)\]]", "", str(line))
+            line = re.sub(r"[\(\[].*?[\)\]]", "", str(line))
             if line and (':' in line) :
                 split_line = line.split(':')
                 script_line = {
@@ -54,25 +63,28 @@ class Scraper:
                     'count': len(split_line[1].split())
                 }
                 script_list.append(script_line)
-        return(script_list)
+        return script_list
 
 
     def get_characters_list(self):
+        """Get list of characters for a given episode"""
         characters_list = list()
-        for ch in self.get_transcript():
-            characters_list.append(ch['name'])
+        for character in self.get_transcript():
+            characters_list.append(character['name'])
         characters_list = list(dict.fromkeys(characters_list))
-        return(characters_list)
+        return characters_list
 
 
     def get_characters_appearance_frequency(self):
+        """ get the number of appearances a character makes for a given episode"""
         characters_list = list()
-        for ch in self.get_transcript():
-            characters_list.append(ch['name'])
-        return(dict(Counter(characters_list)))  
+        for character in self.get_transcript():
+            characters_list.append(character['name'])
+        return dict(Counter(characters_list))
 
 
     def get_character_word_frequency(self):
+        """Get the number of words spoken for each character"""
         episode_word_count = {}
         for character in self.get_characters_list():
             transcripts = self.get_transcript()
@@ -83,30 +95,31 @@ class Scraper:
 
             episode_word_count[character] = total_word_count
 
-        return(episode_word_count)
+        return episode_word_count
 
 
-if __name__ == '__main__':
+def main():
+    """Main function to gather data"""
     full_dict = list()
     # range is from foreverdreaming url
     for episode in range(25306,25308+1):
-        print(f'writing episode '+str(episode))
+        print(f'writing episode {str(episode)}')
         word_frequency_dict = dict()
         url = 'https://transcripts.foreverdreaming.org/viewtopic.php?f=574&t='+str(episode)
 
         request = Scraper(url)
 
-        if request.is_deleted_scenes() != True:
+        if request.is_deleted_scenes() is not True:
             # construct dictionary
             word_frequency_dict['s'] = request.get_season_and_episode()['s']
             word_frequency_dict['e'] = request.get_season_and_episode()['e']
             word_frequency_dict['title'] = request.get_title()
             word_frequency_dict['frequencies'] = request.get_character_word_frequency()
-        
             full_dict.append(word_frequency_dict)
             time.sleep(1)
 
+    with open('scriptdata/statictest.json', 'w') as file:
+        json.dump(full_dict, file)
 
-    with open('scriptdata/statictest.json', 'w') as f:
-        json.dump(full_dict, f)
-
+if __name__ == '__main__':
+    main()
